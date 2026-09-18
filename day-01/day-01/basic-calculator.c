7819b46d77e4b713e8a7c8120537ea11a1fa27c3
baltazar-s <dhejchi0@gmail.com>
2026-09-19 00:51:05 +0300

    add: basic calculator

---
+++ day-01/day-01/basic-calculator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int sayi1;
+    int sayi2;
+
+    printf("Birinci sayi: ");
+    scanf("%d", &sayi1);
+
+    printf("Ikinci sayi: ");
+    scanf("%d", &sayi2);
+
+    printf("Toplam: %d\n", sayi1 + sayi2);
+
+    return 0;
+}
