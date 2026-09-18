b9d7f70b2a9fdee3986fe5cb54c5650838e95823
baltazar-s <dhejchi0@gmail.com>
2026-09-19 00:37:45 +0300

    Day01: Add user age input program
    Implement user age input and output functionality.

---
+++ Day01/age_input.c
@@ -0,0 +1,22 @@
+/*
+    Project: User Age Input
+    Day: 01
+
+    Concepts:
+    - printf
+    - scanf
+    - int
+*/
+
+#include <stdio.h>
+
+int main() {
+    int yas;
+
+    printf("Yasinizi giriniz: ");
+    scanf("%d", &yas);
+
+    printf("Senin yasin: %d\n", yas);
+
+    return 0;
+}
